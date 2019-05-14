5eb00c915ba685318b7b934a72f7da4121e187e6
Henryk Paluch <henryk.paluch@pickering.cz>
2019-05-14 08:19:20 +0000

    Trivial Tcl runner

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
+TCL_INC_DIR := /usr/include/tcl
+TCL_LIB_VER := 8.6
+VALGRIND_FLAGS := --leak-check=full
+
+CPPFLAGS := -I$(TCL_INC_DIR)
 CXXFLAGS := -Wall -g
 LDFLAGS  := -g
+LDLIBS   := -ltcl$(TCL_LIB_VER)
 
 APP := tcl_ex
 
@@ -12,6 +18,11 @@ $(APP).o : $(APP).cpp
 .PHONY: run
 run: $(APP)
 	./$(APP)
+
+.PHONY: valgrind
+valgrind: $(APP)
+	valgrind $(VALGRIND_FLAGS) ./$(APP)
+
 
 .PHONY: clean
 clean:
--- tcl_ex.cpp
+++ tcl_ex.cpp
@@ -5,13 +5,51 @@
 // printf(3)....
 #include <stdio.h>
 
+#include <tcl.h>
+
+// runs TCL commands in tclCommands, returns EXIT_SUCESS or EXIT_FAILURE
+static int ExRunTcl(const char *tclCommands){
+	int rc = EXIT_FAILURE;
+	int err = TCL_OK;
+
+	Tcl_Interp *interp = Tcl_CreateInterp();
+	if (interp == NULL){
+		fprintf(stderr,"Tcl_CreateInterp() returned NULL");
+		goto exit0;
+	}
+
+	err = Tcl_Eval(interp,tclCommands);
+	if ( err != TCL_OK ){
+		fprintf(stderr,"Error calling Tcl_Eval(): %s\n",Tcl_GetStringResult(interp));
+		goto exit2;
+	}
+
+	rc = EXIT_SUCCESS;
+
+	exit2:
+	Tcl_FreeResult(interp);
+
+//	exit1:
+	Tcl_DeleteInterp(interp);
+
+	exit0:
+	return rc;
+}
 
 
 int main(int argc, char **argv)
 {
+	int rc = EXIT_SUCCESS;
 
-	printf("Hello, world!\n");
+	Tcl_FindExecutable(argv[0]);
 
-	return EXIT_SUCCESS;
+	rc = ExRunTcl("puts \"Hello, world!\"");
+
+	if (rc == EXIT_SUCCESS){
+		rc = ExRunTcl("puts \"Hello, world2!\"");
+	}
+
+	Tcl_Finalize();
+	return rc;
 }
 
